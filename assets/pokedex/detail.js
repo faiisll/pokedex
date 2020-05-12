993d3dc00fc5a27e5432d7486831b37a751be6a2
faiisll <faisal.ayas72@gmail.com>
2020-05-13 05:26:47 +0700

    first commit

---
+++ assets/pokedex/detail.js
@@ -0,0 +1,46 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const id = urlParams.get('id');
+console.log(id);
+
+const setDataPokemon = async () =>{
+    let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    data = await data.json();
+    let stat = '';
+    let type = '';
+
+    //set image
+    document.getElementById('img').innerHTML = await `<img src="${data.sprites.front_default}" class="rounded-circle bg-dark">`;
+
+    // set name
+    document.getElementById('name').innerHTML = await data.name;
+
+    //set type
+    data.types.map(t => type = type + `<a href="#" class="btn btn-sm btn-primary">${t.type.name}</a>`);
+    document.getElementById('type').innerHTML = await type;
+
+    //set stats
+    data.stats.map(t => stat = stat + `<div class="col-4 col-md-1"><span class="heading">${t.base_stat}</span><span class="description">${t.stat.name}</span></div>`);
+    document.getElementById('stat').innerHTML = await stat;
+
+};
+
+const setDescPokemon = async () => {
+    let data = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    data = await data.json();
+    let desc = [];
+    let str = '';
+
+    desc = await data.flavor_text_entries.filter(dt => dt.language.name === 'en');
+    for(let i = 0 ; i < 2 ; i++){
+        str = await str + `${desc[i].flavor_text} `;
+    }
+    document.getElementById('desc').innerHTML = await str;
+    console.log(await desc);
+}
+
+setTimeout(async () =>{
+    await setDataPokemon();
+    await setDescPokemon();
+}, 1);
---
+++ assets/pokedex/index.js
@@ -0,0 +1,86 @@
+const getList = async (offset = 1, limit = 20) =>{
+    let data = await {};
+    let con = [];
+    let isi = '';
+    
+    for(let i = offset; i <= limit; i++){
+        data = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        data = await data.json();
+        await con.push(data);
+    }
+
+    await con.map((p) => {
+        isi = isi + `<tr>
+        <th scope="row">
+          <div class="media align-items-center">
+            <a href="#" class="avatar rounded-circle mr-3">
+              <img alt="Image placeholder" src="${p.sprites.front_default}">
+            </a>
+            <div class="media-body">
+              <span class="mb-0 text-sm">${p.name}</span>
+            </div>
+          </div>
+        </th>
+        <td>
+          No.${p.order}
+        </td>
+        <td>
+            ${p.types.map( t => `<span class="badge badge-dot mr-1"><i class="bg-dark"></i>${t.type.name}</span>`)}
+        </td>
+
+        <td class="text-right">
+          <a class="btn btn-sm btn-icon-only text-light" href="detail.html?id=${p.id}" role="button">
+            <i class="fas fa-info"></i>
+          </a>
+        </td>
+      </tr>`;
+    });
+    
+    
+    document.getElementById('tb-poke').innerHTML = await isi;
+
+};
+
+let lim = 20;
+let off = 1;
+
+const next = () => {
+    let btn = document.getElementById('next');
+    let btn2 = document.getElementById('prev');
+    if(lim <= 964){
+        lim = lim + 20;
+        off = off + 20;
+
+        setTimeout(async () =>{
+            await btn.classList.add('disabled');
+            await getList(off, lim);
+            await btn.classList.remove('disabled');
+            await btn2.classList.remove('disabled');
+        }, 1);
+    }else{
+        alert('You have reach the last page');
+        btn.classList.add('disabled');
+    }
+};
+
+
+//has a bug, when off < 1 the prev button still active (not disabled)
+const prev = () => {
+    let btn = document.getElementById('prev');
+    if(off >= 1){
+        lim = lim - 20;
+        off = off - 20;
+
+        setTimeout(async () =>{
+            await btn.classList.add('disabled');
+            await getList(off, lim);
+            await btn.classList.remove('disabled');
+        }, 1);
+    }else{
+        alert('You have reach the last page');
+        btn.classList.add('disabled');
+    }
+}
+setTimeout(async () =>{
+    await getList();
+}, 1);
